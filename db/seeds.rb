faa902952e3ccf6d9500f78b9f0e8868a09f9828
joao mateus <jaomateus@gmail.com>
2024-11-14 13:42:24 +0100

    finish first part

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts "Deleting gardens..."
+Garden.destroy_all
+
+puts "Creating gardens..."
+Garden.create!(name: "Vondelpark", banner_url: "https://www.amsterdam.nl/publish/pages/881036/vondelpark_960x415.jpg")
+puts "Created Vondelpark"
+
+Garden.create!(name: "Westerpark", banner_url: "https://www.amsterdam.nl/publish/pages/1046944/68183-westerpark-25okt2022-e-v-eis_2_.jpg")
+puts "Created Vondelpark"
+
+puts "Finished creating gardens!"
